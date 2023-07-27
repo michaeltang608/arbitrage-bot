5942b76752d566fa9191c4c7e9d4d34d8defc0bf
Michael Tang <573940273@qq.com>
2023-07-27 09:29:15 +0800

    m

--- server/backend/schedule.go
+++ server/backend/schedule.go
@@ -5,26 +5,31 @@ import (
 	"github.com/robfig/cron/v3"
 	"time"
 	"ws-quant/cex/models"
+	"ws-quant/pkg/feishu"
 	"ws-quant/pkg/mapper"
 )
 
 func (bs *backendServer) scheduleJobs() {
 	c := cron.New()
 	//每隔1h 清零一次并通知
-	_, _ = c.AddFunc("0/5 * * * *", func() {
+	_, _ = c.AddFunc("0/2 * * * *", func() {
 		//feishu.Send(fmt.Sprintf("前1h的 max是%.2f ", bs.curMax))
 		maxOkMarginFutureDiff := fmt.Sprintf("%.2f", bs.curMaxMarginFuture)
-		//feishu.Send(fmt.Sprintf("前1h的 margin/future max是%.2f ", bs.curMaxMarginFuture))
-		m := &models.Oppor{
-			InstId:  "okMarginFuture",
-			Cex:     "ok",
-			MaxDiff: maxOkMarginFutureDiff,
-			Created: time.Now(),
+		if bs.curMaxMarginFuture >= 1.0 {
+			m := &models.Oppor{
+				InstId:  "okMarginFuture",
+				Cex:     "ok",
+				MaxDiff: maxOkMarginFutureDiff,
+				Created: time.Now(),
+			}
+			err := mapper.Insert(bs.db, m)
+			if err != nil {
+				log.Error("insert err=", err)
+			}
+			feishu.Send(fmt.Sprintf("前2m的 margin/future max是%.2f ", bs.curMaxMarginFuture))
+
 		}
-		err := mapper.Insert(bs.db, m)
-		if err != nil {
-			log.Error("insert err=", err)
-		}
+
 		bs.curMax = 0.0
 		bs.curMaxMarginFuture = 0.0
 
