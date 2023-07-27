39eb96ef50c58e87046407b2002bd332032bc212
Michael Tang <573940273@qq.com>
2023-07-27 09:16:19 +0800

    m

--- server/backend/schedule.go
+++ server/backend/schedule.go
@@ -3,15 +3,32 @@ package backend
 import (
 	"fmt"
 	"github.com/robfig/cron/v3"
+	"time"
+	"ws-quant/cex/models"
 	"ws-quant/pkg/dingding"
+	"ws-quant/pkg/mapper"
 )
 
 func (bs *backendServer) scheduleJobs() {
 	c := cron.New()
 	//每隔1h 清零一次并通知
-	_, _ = c.AddFunc("0 * * * *", func() {
-		dingding.Send(fmt.Sprintf("前1h的 max是%.2f ", bs.curMax))
-		//feishu.Send(fmt.Sprintf("前1h的 margin future max是%.2f ", bs.curMaxMarginFuture))
+	_, _ = c.AddFunc("0/5 * * * *", func() {
+		maxDiff := fmt.Sprintf("%.2f ", bs.curMax)
+		if bs.curMax >= 1.0 {
+			dingding.Send("maxDiff=" + maxDiff)
+			err := mapper.Insert(bs.db, &models.Oppor{
+				InstId:  "margins",
+				Cex:     "bi-ok",
+				MaxDiff: maxDiff,
+				Created: time.Now(),
+			})
+
+			if err != nil {
+				log.Error("insert err=", err)
+				dingding.Send(fmt.Sprintf("insert error:%v", err.Error()))
+			}
+		}
+
 		bs.curMax = 0.0
 		bs.curMaxMarginFuture = 0.0
 
